042ee169c0c3590796bedc6d3c62e0093cbb4ebf
thk34 <103599660+thk34@users.noreply.github.com>
2023-02-01 20:56:37 +0100

    Add files via upload

---
+++ thk34-the-ultimate-youtube-music-playlist-downloader.py
@@ -0,0 +1,17 @@
+# made by Thk34: https://github.com/thk34
+from pytube import YouTube
+from pytube import Playlist
+import os
+import moviepy.editor as mp
+import re
+
+playlist = Playlist("put the link of the playlist here, it must be unlisted or in public but not in private")
+
+playlist.video_urls
+for url in playlist:
+    print(url)
+
+for url in playlist:
+   YouTube(url).streams.filter(only_audio=True).first().download()
+   
+os.system('cmd /k "ren *.mp4 *.mp3"')
